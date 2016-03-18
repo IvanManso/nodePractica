dfd90bf237665e1ceff187d19e3398e1ae9a912e
Iván Manso <ivan.manso@babel-si.local>
2016-03-18 10:35:06 +0100

    Añado los archivos actuales de los comentarios con APIDOC.

---
+++ nodepop/apidoc/api_data.js
@@ -0,0 +1,21 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/api/v1/anuncio",
+    "title": "",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "{\n  \"result\": true\n}\n{\n \"nombre\": \"Bicicleta\",\n  \"venta\": true,\n  \"precio\": 230.15,\n  \"foto\": \"bici.jpg\",\n  \"tags\": [\"lifestyle\", \"motor\"]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "routes/api/v1/anuncio.js",
+    "group": "D__Users_ivan_manso_BABEL_SI_Desktop_nodePractica_nodepop_routes_api_v1_anuncio_js",
+    "groupTitle": "D__Users_ivan_manso_BABEL_SI_Desktop_nodePractica_nodepop_routes_api_v1_anuncio_js",
+    "name": "GetApiV1Anuncio"
+  }
+] });
